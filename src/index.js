10c3945742de87709249db7f37e0d80ce6bf9535
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-13 02:08:21 +0500

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    error => console.log(error)
-  );
-  return <div>Hello, World!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
+    return <div>You are in northern hemisphere</div>;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
